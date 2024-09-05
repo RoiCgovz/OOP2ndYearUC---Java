cfc5759c1638fd871df02b427c41ec2cec94f703
Roi <150252081+RoiCgovz@users.noreply.github.com>
2024-09-05 20:50:12 +0800

    Delete LibraryManagementSystem/MainProgram.java

--- LibraryManagementSystem/MainProgram.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-import java.lang.*;
-
-public class MainProgram {
-   public static void main(String[] args)
-   ArrayList<Books> books = new ArrayList<Books>();
-   books.add("Animal Stories", "Maria Hoey", "978-1-60309-502-0", 20);
-   books.add("Ashes", "Alvaro Ortiz", c  
-}
