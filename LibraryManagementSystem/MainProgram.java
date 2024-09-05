07c355c84a6b45860cb9cd4df96115938c7dc3ac
Roi <150252081+RoiCgovz@users.noreply.github.com>
2024-09-04 19:22:13 -0700

    Add files via upload

---
+++ LibraryManagementSystem/MainProgram.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.lang.*;
+
+public class MainProgram {
+   public static void main(String[] args)
+   ArrayList<Books> books = new ArrayList<Books>();
+   books.add("Animal Stories", "Maria Hoey", "978-1-60309-502-0", 20);
+   books.add("Ashes", "Alvaro Ortiz", c  
+}
